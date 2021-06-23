7f15c751eb067eabfdca516ae3fb18a3e5bdf452
Nebual <ben1120@gmail.com>
2021-06-23 03:51:53 -0700

    Add Sandbox.Hooks.Undos.OnAddUndo event

--- code/Game.cs
+++ code/Game.cs
@@ -286,6 +286,18 @@ partial class SandboxGame : GameManager
 
 namespace Sandbox.Hooks
 {
+	public static partial class Undos
+	{
+		public static event Action<Func<string>, Entity> OnAddUndo;
+
+		// Add an "Undoable" lambda. Should return the string to show in the toast,
+		// or empty string if the undoable is redundant and should be skipped over (eg. if the weld was already removed)
+		public static void AddUndo(Func<string> undo, Entity owner)
+		{
+			OnAddUndo?.Invoke(undo, owner);
+		}
+	}
+
 	public static partial class Entities
 	{
 		public static event Action<IEntity, IEntity> OnSpawned;
