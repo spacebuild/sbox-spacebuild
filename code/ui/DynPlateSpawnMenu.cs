345138f82d851af7881fb8668381784779e81edf
Nebual <ben1120@gmail.com>
2021-07-03 03:18:48 -0700

    DynPlate Spawnlist: allow custom sizes, store em

--- code/ui/DynPlateSpawnMenu.cs
+++ code/ui/DynPlateSpawnMenu.cs
@@ -1,5 +1,7 @@
-
-using MinimalExtended;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Sandbox;
 using Sandbox.UI;
 using Sandbox.UI.Construct;
@@ -8,11 +10,11 @@ using Sandbox.UI.Tests;
 namespace Sandbox
 {
 	[Library]
-	public class DynPlateSpawnMenu : IAutoload
+	public class DynPlateSpawnMenu
 	{
 		public DynPlateSpawnMenu()
 		{
-			if ( Host.IsClient ) {
+			if ( Game.IsClient ) {
 				Log.Info( "[Client] DynPlateSpawnMenu created" );
 
 				SandboxHud.OnHudLoaded -= Initialize;
@@ -36,9 +38,13 @@ namespace Sandbox
 	{
 		private struct DynPlateDimensions
 		{
+			[JsonInclude]
 			public int x;
+			[JsonInclude]
 			public int y;
+			[JsonInclude]
 			public int z;
+			[JsonInclude]
 			public int scale;
 			public DynPlateDimensions( int x, int y, int z, int scale )
 			{
@@ -48,31 +54,85 @@ namespace Sandbox
 				this.scale = scale;
 			}
 		}
-		VirtualScrollPanel Canvas;
+		private VirtualScrollPanel Canvas;
+		private HashSet<DynPlateDimensions> plates = new();
 
 		public PlateList()
 		{
-			AddClass( "spawnpage" );
 			AddClass( "dynplates" );
 			AddChild( out Canvas, "canvas" );
 
 			Canvas.Layout.Columns = 16;
-			Canvas.Layout.ItemSize = new Vector2( 64, 64 );
+			Canvas.Layout.ItemWidth = 64;
+			Canvas.Layout.ItemHeight = 64;
 			Canvas.OnCreateCell = ( cell, data ) => {
 				var entry = (DynPlateDimensions)data;
 				var btn = cell.Add.Button( $"{entry.x}x{entry.y}" );
 				btn.AddClass( "icon" );
-				btn.AddEventListener( "onclick", () => ConsoleSystem.Run( "spawn_dynplate", entry.x, entry.y, entry.z, entry.scale ) );
+				btn.AddEventListener( "onclick", () =>
+					ConsoleSystem.Run( "spawn_dynplate", entry.x, entry.y, entry.z, entry.scale )
+				);
 			};
 
-			for ( var x = 1; x <= 12; x++ ) {
-				for ( var y = 12; y <= 12 * 8; y += 12 ) {
-					Canvas.AddItem( new DynPlateDimensions( x * 12, y, 1, 64 ) );
-				}
-				for ( var y = 12 * 8 + 24; y <= 12 * 8 + 24 * 8; y += 24 ) {
-					Canvas.AddItem( new DynPlateDimensions( x * 12, y, 1, 64 ) );
-				}
+			var storedSpawnListSerialized = FileSystem.Data.ReadAllText("dynplate_spawnlist.json");
+			if ( storedSpawnListSerialized != null ) {
+				plates = JsonSerializer.Deserialize<HashSet<DynPlateDimensions>>( storedSpawnListSerialized );
 			}
+			else {
+				plates.Add( new DynPlateDimensions( 12, 12, 1, 64 ) );
+				plates.Add( new DynPlateDimensions( 12, 24, 1, 64 ) );
+				plates.Add( new DynPlateDimensions( 24, 24, 1, 64 ) );
+				plates.Add( new DynPlateDimensions( 24, 48, 1, 64 ) );
+				plates.Add( new DynPlateDimensions( 48, 48, 1, 64 ) );
+				plates.Add( new DynPlateDimensions( 48, 96, 1, 64 ) );
+				plates.Add( new DynPlateDimensions( 96, 96, 1, 64 ) );
+			}
+
+			Canvas.SetItems( GetOrderedPlates() );
+
+			var inputContainer = Add.Panel( "input-container" );
+			var dimensionsContainer = inputContainer.Add.Panel( "dimensions-container" );
+			dimensionsContainer.Add.Label( "Rectangles", "header" );
+
+			var lengthContainer = dimensionsContainer.Add.Panel( "flex-column" );
+			lengthContainer.Add.Label( "Length", "input-label" );
+			var lengthEntry = lengthContainer.Add.MenuTextEntry( "48" );
+
+			var widthContainer = dimensionsContainer.Add.Panel( "flex-column" );
+			widthContainer.Add.Label( "Width", "input-label" );
+			var widthEntry = widthContainer.Add.MenuTextEntry( "24" );
+
+			var heightContainer = dimensionsContainer.Add.Panel( "flex-column" );
+			heightContainer.Add.Label( "Height", "input-label" );
+			var heightEntry = heightContainer.Add.MenuTextEntry( "3" );
+
+			var textureSizeContainer = dimensionsContainer.Add.Panel( "flex-column" );
+			textureSizeContainer.Add.Label( "Texture Size", "input-label" );
+			var textureSizeEntry = textureSizeContainer.Add.MenuTextEntry( "64" );
+
+			var spawnButton = dimensionsContainer.Add.Button( "Spawn DynPlate", () => {
+				if ( !int.TryParse( lengthEntry.Text, out int x )
+					|| !int.TryParse( widthEntry.Text, out int y )
+					|| !int.TryParse( heightEntry.Text, out int z )
+					|| !int.TryParse( textureSizeEntry.Text, out int texSize ) ) {
+					return;
+				}
+				ConsoleSystem.Run( "spawn_dynplate", x, y, z, texSize );
+				var added = plates.Add( new DynPlateDimensions( x, y, z, texSize ) );
+				if ( added ) {
+					FileSystem.Data.WriteAllText("dynplate_spawnlist.json", JsonSerializer.Serialize( plates ));
+					Canvas.SetItems( GetOrderedPlates() );
+				}
+			} );
+		}
+
+		private IEnumerable<object> GetOrderedPlates()
+		{
+			return plates.OrderBy( plate => plate.x )
+				.ThenBy( plate => plate.y )
+				.ThenBy( plate => plate.z )
+				.ThenBy( plate => plate.scale )
+				.Select( plate => (object)plate );
 		}
 	}
 }
