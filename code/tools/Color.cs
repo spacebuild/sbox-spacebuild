SupinePandora43/sandbox

Add Toolgun imported from Gmod

code/tools/Color.cs
@@ -31,7 +31,7 @@ namespace Sandbox.Tools
 
 				modelEnt.RenderColor = Color.Random.ToColor32();
 
-				CreateHitEffects( tr.EndPos );
+				CreateHitEffects( tr.EndPos, tr.Normal );
 			}
 		}
 	}

code/tools/Lamp.cs
@@ -48,7 +48,7 @@
 				if ( !tr.Hit || !tr.Entity.IsValid() )
 					return;
 
-				CreateHitEffects( tr.EndPos );
+				CreateHitEffects( tr.EndPos, tr.Normal );
 
 				if ( tr.Entity is LampEntity lamp )
 				{

code/tools/Light.cs
@@ -49,7 +49,7 @@
 				if ( !tr.Hit || !tr.Entity.IsValid() )
 					return;
 
-				CreateHitEffects( tr.EndPos );
+				CreateHitEffects( tr.EndPos, tr.Normal );
 
 				if ( tr.Entity is LightEntity )
 				{

code/tools/Remover.cs
@@ -27,7 +27,7 @@
 				if ( tr.Entity is Player )
 					return;
 
-				CreateHitEffects( tr.EndPos );
+				CreateHitEffects( tr.EndPos, tr.Normal );
 
 				if ( tr.Entity.IsWorld )
 					return;

code/tools/Resizer.cs
@@ -44,7 +44,7 @@ namespace Sandbox.Tools
 
 				if ( Input.Pressed( InputButton.Attack1 ) || Input.Pressed( InputButton.Attack2 ) )
 				{
-					CreateHitEffects( tr.EndPos );
+					CreateHitEffects( tr.EndPos, tr.Normal, true );
 				}
 			}
 		}

code/tools/Thruster.cs
@@ -58,7 +58,7 @@
 				if ( attached && tr.Entity is not Prop )
 					return;
 
-				CreateHitEffects( tr.EndPos );
+				CreateHitEffects( tr.EndPos, tr.Normal );
 
 				if ( tr.Entity is ThrusterEntity )
 				{

code/tools/Weld.cs
@@ -70,7 +70,7 @@
 					return;
 				}
 
-				CreateHitEffects( tr.EndPos );
+				CreateHitEffects( tr.EndPos, tr.Normal );
 			}
 		}
 

spacebuild/sbox-spacebuild

code/tools/Color.cs
@@ -27,7 +27,7 @@ namespace Sandbox.Tools
 
 				modelEnt.RenderColor = Color.Random;
 
-				CreateHitEffects( tr.EndPosition );
+				CreateHitEffects( tr.EndPosition, tr.Normal );
 			}
 		}
 	}

code/tools/Lamp.cs
@@ -43,7 +43,7 @@
 				if ( !tr.Hit || !tr.Entity.IsValid() )
 					return;
 
-				CreateHitEffects( tr.EndPosition );
+				CreateHitEffects( tr.EndPosition, tr.Normal );
 
 				if ( tr.Entity is LampEntity lamp )
 				{

code/tools/Light.cs
@@ -46,7 +46,7 @@ public partial class LightTool : BaseTool
 			if ( !tr.Hit || !tr.Entity.IsValid() )
 				return;
 
-			CreateHitEffects( tr.EndPosition );
+			CreateHitEffects( tr.EndPosition, tr.Normal );
 
 			if ( tr.Entity is LightEntity )
 			{

code/tools/Remover.cs
@@ -21,7 +21,7 @@
 				if ( tr.Entity is Player )
 					return;
 
-				CreateHitEffects( tr.EndPosition );
+				CreateHitEffects( tr.EndPosition, tr.Normal );
 
 				if ( tr.Entity.IsWorld )
 					return;

code/tools/Resizer.cs
@@ -57,7 +57,7 @@ namespace Sandbox.Tools
 
 				if ( Input.Pressed( "attack1" ) || Input.Pressed( "attack2" ) || reset )
 				{
-					CreateHitEffects( tr.EndPosition );
+					CreateHitEffects( tr.EndPosition, tr.Normal, true );
 				}
 			}
 		}

code/tools/Thruster.cs
@@ -53,7 +53,7 @@
 				if ( attached && tr.Entity is not Prop )
 					return;
 
-				CreateHitEffects( tr.EndPosition );
+				CreateHitEffects( tr.EndPosition, tr.Normal );
 
 				if ( tr.Entity is ThrusterEntity )
 				{

code/tools/Weld.cs
@@ -65,7 +65,7 @@
 					return;
 				}
 
-				CreateHitEffects( tr.EndPosition );
+				CreateHitEffects( tr.EndPosition, tr.Normal );
 			}
 		}
 