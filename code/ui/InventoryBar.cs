SupinePandora43/sandbox

Add SandboxPlayer.SuppressScrollWheelInventory

code/ui/InventoryBar.cs
@@ -45,7 +45,7 @@ public class InventoryBar : Panel
 	[Event( "buildinput" )]
 	public void ProcessClientInput( InputBuilder input )
 	{
-		var player = Local.Pawn as Player;
+		var player = Local.Pawn as SandboxPlayer;
 		if ( player == null )
 			return;
 
@@ -65,7 +65,9 @@ public class InventoryBar : Panel
 		if ( input.Pressed( InputButton.Slot5 ) ) SetActiveSlot( input, inventory, 4 );
 		if ( input.Pressed( InputButton.Slot6 ) ) SetActiveSlot( input, inventory, 5 );
 
-		if ( input.MouseWheel != 0 ) SwitchActiveSlot( input, inventory, -input.MouseWheel );
+		if ( !player.SuppressScrollWheelInventory && input.MouseWheel != 0 ) {
+			SwitchActiveSlot( input, inventory, -input.MouseWheel );
+		}
 	}
 
 	private static void SetActiveSlot( InputBuilder input, IBaseInventory inventory, int i )

spacebuild/sbox-spacebuild

code/ui/InventoryBar.cs
@@ -49,7 +49,7 @@ public class InventoryBar : Panel
 	[Event.Client.BuildInput]
 	public void ProcessClientInput()
 	{
-		var player = Game.LocalPawn as Player;
+		var player = Game.LocalPawn as SandboxPlayer;
 		if ( player == null )
 			return;
 
@@ -72,7 +72,9 @@ public class InventoryBar : Panel
 		if ( Input.Pressed( "slot8" ) ) SetActiveSlot( inventory, 7 );
 		if ( Input.Pressed( "slot9" ) ) SetActiveSlot( inventory, 8 );
 
-		if ( Input.MouseWheel != 0 ) SwitchActiveSlot( inventory, -Input.MouseWheel );
+		if ( !player.SuppressScrollWheelInventory && Input.MouseWheel != 0 ) {
+			SwitchActiveSlot( inventory, -Input.MouseWheel );
+		}
 	}
 
 	private static void SetActiveSlot( IBaseInventory inventory, int i )