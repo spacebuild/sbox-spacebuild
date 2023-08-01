SupinePandora43/sandbox

Resizer should use continous CreateHitEffects

code/tools/Resizer.cs
@@ -42,10 +42,7 @@ namespace Sandbox.Tools
 					tr.Entity.PhysicsGroup.Wake();
 				}
 
-				if ( Input.Pressed( InputButton.Attack1 ) || Input.Pressed( InputButton.Attack2 ) )
-				{
-					CreateHitEffects( tr.EndPos, tr.Normal, true );
-				}
+				CreateHitEffects( tr.EndPos, tr.Normal, true );
 			}
 		}
 	}

spacebuild/sbox-spacebuild

code/tools/Resizer.cs
@@ -55,10 +55,7 @@ namespace Sandbox.Tools
 					}
 				}
 
-				if ( Input.Pressed( "attack1" ) || Input.Pressed( "attack2" ) || reset )
-				{
-					CreateHitEffects( tr.EndPosition, tr.Normal, true );
-				}
+				CreateHitEffects( tr.EndPosition, tr.Normal, true );
 			}
 		}
 	}