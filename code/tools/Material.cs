b93209c531079ad1adb28393887ffe6ea6d12194
Nebual <ben1120@gmail.com>
2023-08-30 02:32:42 -0700

    Material Tool: Support Cloud Materials

--- code/tools/Material.cs
+++ code/tools/Material.cs
@@ -3,7 +3,7 @@ using Sandbox.UI;
 
 namespace Sandbox.Tools
 {
-	[Library( "tool_material", Title = "Material", Description = "Primary: Set Material Override\nSecondary: Change Model's MaterialGroup (if supported)", Group = "construction" )]
+	[Library( "tool_material", Title = "Material", Description = "Primary: Set Material Override\nSecondary: Change Model's MaterialGroup (if supported)\nReload: Clear Material Override", Group = "construction" )]
 	public partial class MaterialTool : BaseTool
 	{
 		[ConVar.ClientData( "tool_material_material" )]
@@ -29,6 +29,10 @@ namespace Sandbox.Tools
 				}
 				else if ( Input.Pressed( "attack2" ) )
 				{
+					if ( modelEnt.MaterialGroupCount == 0 )
+					{
+						return;
+					}
 					modelEnt.SetMaterialGroup( modelEnt.GetMaterialGroup() + 1 );
 					if ( modelEnt.GetMaterialGroup() == 0 )
 					{
@@ -38,18 +42,57 @@ namespace Sandbox.Tools
 
 					CreateHitEffects( tr.EndPosition, tr.Normal, true );
 				}
+				else if ( Input.Pressed( "reload" ) )
+				{
+					modelEnt.SetClientMaterialOverride( "" );
+
+					CreateHitEffects( tr.EndPosition, tr.Normal );
+				}
 			}
 		}
 
 		[ClientRpc]
-		public static void SetEntityMaterialOverride( ModelEntity modelEnt, string material )
+		public static async void SetEntityMaterialOverride( ModelEntity modelEnt, string material )
 		{
 			if ( Game.IsClient )
 			{
-				modelEnt?.SceneObject?.SetMaterialOverride( Material.Load( material ) );
+				if ( material != "" && !material.EndsWith( ".vmat" ) )
+				{
+					var package = await Package.FetchAsync( material, false, true );
+					if ( package == null )
+					{
+						Log.Warning( $"Material: Tried to load material package {material} - which was not found" );
+						return;
+					}
+
+					await package.MountAsync( false );
+					material = package.GetCachedMeta( "SingleAssetSource", "" );
+					if ( material == "" )
+					{
+						Log.Warning( $"Material2: package {material} lacks SingleAssetSource - is it actually a Material?" );
+						return;
+					}
+				}
+				// modelEnt.SetMaterialOverride does not seem to work until the prop is touched, yet SceneObject.SetMaterialOverride only works _until_ its touched, so set both
+				if ( material == "" )
+				{
+					modelEnt?.ClearMaterialOverride();
+					modelEnt?.SceneObject?.ClearMaterialOverride();
+				}
+				else
+				{
+					modelEnt?.SetMaterialOverride( material );
+					modelEnt?.SceneObject?.SetMaterialOverride( Material.Load( material ) );
+				}
 			}
 		}
 
+		[Event( "spawnlists.initialize" )]
+		public static async void SpawnlistsInitialize()
+		{
+			var collectionLookup = await Package.FetchAsync( "sugmatextures/sugmatextures", false, true );
+			ModelSelector.AddToSpawnlist( "material", collectionLookup.PackageReferences );
+		}
 
 		public override void CreateToolPanel()
 		{
--- code/ui/MaterialSelector.cs
+++ code/ui/MaterialSelector.cs
@@ -16,20 +16,23 @@ namespace Sandbox.UI
 			Canvas.Layout.AutoColumns = true;
 			Canvas.Layout.ItemWidth = 64;
 			Canvas.Layout.ItemHeight = 64;
-			Canvas.OnCreateCell = ( cell, data ) =>
+			Canvas.OnCreateCell = async ( cell, data ) =>
 			{
 				var file = (string)data;
-				// todo: Cloud.Material would be neat, but does not appear to work
-				var material = Material.Load( file );
-
-				var sceneWorld = new SceneWorld();
-				var mod = new SceneObject( sceneWorld, Cloud.Model( "https://asset.party/drakefruit/cube32" ), Transform.Zero );
-				mod.SetMaterialOverride( material );
-
-				var sceneLight = new SceneLight( sceneWorld, Vector3.Up * 45.0f, 300.0f, Color.White * 30.0f );
-
-				ScenePanel panel = cell.Add.ScenePanel( sceneWorld, Vector3.Up * 68, new Angles( 90, 0, 0 ).ToRotation(), 45, "icon" );
-				panel.RenderOnce = true;
+				Panel panel;
+				if ( file.EndsWith( ".vmat" ) )
+				{
+					panel = AddLocalMaterialIcon( cell, file );
+				}
+				else
+				{
+					panel = await AddCloudMaterialIcon( cell, file );
+					if ( panel == null )
+					{
+						return;
+					}
+				}
+				panel.Tooltip = file;
 
 				panel.AddEventListener( "onclick", () =>
 				{
@@ -42,7 +45,7 @@ namespace Sandbox.UI
 
 			foreach ( var file in spawnList )
 			{
-				if ( !FileSystem.Mounted.FileExists( file + "_c" ) )
+				if ( file.EndsWith( ".vmat" ) && !FileSystem.Mounted.FileExists( file + "_c" ) )
 				{
 					continue;
 				}
@@ -50,6 +53,34 @@ namespace Sandbox.UI
 			}
 			// VirtualScrollPanel doesn't have a valid height (subsequent children overlap it within flex-direction: column) so calculate it manually
 			Style.Height = (64 + 6) * (int)Math.Ceiling( spawnList.Count() / 5f );
+		}
+
+		private Panel AddLocalMaterialIcon( Panel cell, string file )
+		{
+			var material = Material.Load( file );
+
+			var sceneWorld = new SceneWorld();
+			var mod = new SceneObject( sceneWorld, Cloud.Model( "https://asset.party/drakefruit/cube32" ), Transform.Zero );
+			mod.SetMaterialOverride( material );
+
+			var sceneLight = new SceneLight( sceneWorld, Vector3.Up * 45.0f, 300.0f, Color.White * 30.0f );
+
+			ScenePanel panel = cell.Add.ScenePanel( sceneWorld, Vector3.Up * 68, new Angles( 90, 0, 0 ).ToRotation(), 45, "icon" );
+			panel.RenderOnce = true;
+			return panel;
+		}
+
+		private async Task<Panel> AddCloudMaterialIcon( Panel cell, string file )
+		{
+			var package = await Package.FetchAsync( file, true, true );
+			if ( package == null )
+			{
+				Log.Warning( $"MaterialSelector: Tried to load material package {file} - which was not found" );
+				return null;
+			}
+
+			var panel = cell.Add.Image( package.Thumb, "icon" );
+			return panel;
 		}
 	}
 }
