3e1884f9c21803eb7ff036faa2a04345d15d470b
Nebual <ben1120@gmail.com>
2023-09-06 01:42:03 -0700

    WhatIsThat tool: print to client's console Fixes #20

--- code/tools/WhatIsThat.cs
+++ code/tools/WhatIsThat.cs
@@ -1,9 +1,6 @@
-using Sandbox.UI;
-using System.Xml;
-
 namespace Sandbox.Tools
 {
-	[Library( "tool_whatisthat", Title = "What is that?", Description = "Prop identificationifier (prints to console because what is chat?)", Group = "construction" )]
+	[Library( "tool_whatisthat", Title = "What is that?", Description = "Prop identificationifier", Group = "construction" )]
 	public partial class WhatIsThatTool : BaseTool
 	{
 		public override void Simulate()
@@ -33,25 +30,31 @@ namespace Sandbox.Tools
 					var prop = tr.Entity as Prop;
 					if ( prop.IsValid() )
 					{
-						message += $" {prop.Model.Name}";
+						message += $" {prop.Model.Name},\n";
 						if ( prop.PhysicsBody.IsValid() )
 						{
-							message += $" weighing {prop.PhysicsBody.Mass}";
+							message += $" weighing {prop.PhysicsBody.Mass},";
 						}
 
 						var playerOwner = prop.GetPlayerOwner();
 						if ( playerOwner.IsValid() )
 						{
 							var ownerClient = Game.Clients.FirstOrDefault( c => c.NetworkIdent == (playerOwner?.Owner?.NetworkIdent ?? playerOwner?.NetworkIdent) );
-							message += $" owned by {ownerClient?.Name ?? playerOwner.ToString()}";
+							message += $" owned by {ownerClient?.Name ?? playerOwner.ToString()},";
 						}
 					}
+					message += $" trace position {tr.EndPosition}";
 
-					// prints to console, and to hint feed (for us only)
-					Log.Info( message );
+					LogClientside( To.Single( Owner.Client ), message.Replace( "\n", "" ) );
 					HintFeed.AddHint( To.Single( Owner.Client ), "question_mark", message );
 				}
 			}
+		}
+
+		[ClientRpc]
+		public static void LogClientside( string message )
+		{
+			Log.Info( message );
 		}
 	}
 }
